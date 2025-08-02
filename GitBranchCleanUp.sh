74b0214426b7b88fe5a226a431e30344dca10cd5
sachin <sachinram2510@gmail.com>
2025-08-02 08:14:06 +0000

    Final bash script

--- GitBranchCleanUp.sh
+++ GitBranchCleanUp.sh
@@ -1,4 +1,4 @@
-#! bin/bash
+#! /bin/bash
 
 # Get All git braches
 # List all branches
@@ -6,6 +6,57 @@
 
 baseBranch="main"
 
-echo "All available local Branches are"
 
-git branch | awk (print $1 == "*"?$2:$1) 
+while true;do
+    main
+    if["$breakloop"==1];then
+        break
+    fi
+done
+
+
+
+main() 
+{
+    echo "Welcome to Git"
+    sleep 3
+    echo "Enter your choice"
+    echo "1. List all branches"
+    echo "2. Remove all merged branches with $baseBranch"
+    echo "3. Exit"
+    read choice
+
+    case "$choice" in 
+
+        1) 
+            echo "Listing all the exisitng branches..."
+            braches
+            ;;
+        2)
+            echo "Removing all the merged Branched with $baseBranch..."
+            removeBranches
+            ;;
+        3)
+            echo "Exiting the shell.."
+            breakloop=1
+            ;;
+        *)
+            echo "Invalid Choice.."
+            echo "Please choose a valid one.."
+            ;;
+    esac
+}
+
+
+branches()
+{
+    sleep 1
+    git branch | awk `{print $1 == "*" ? $2:$1 }`
+}
+
+removeBranches()
+{
+    sleep 2
+
+    git branch --merged "$baseBranch"| grep -v '^\*' | grep -v "$baseBranch\$" | xargs -n 1 git branch -d
+}
